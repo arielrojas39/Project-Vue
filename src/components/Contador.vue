3bcc63cf92f31902ce4d4d496c8abd7432a6d89c
arielrojas39 <rojas.ariel.397@gmail.com>
2024-09-19 12:54:21 -0300

    Creamos componente contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Hola mundo!</h1>
+  <p>{{count}}</p>
+  <button @click="incrementar">Incrementar</button>
+  <button @click="decrementar">Decrementar</button>
+</template>
+
+<script setup>
+  import {ref} from 'vue'
+  const count = ref(0);
+  const incrementar = () => {
+    count.value++
+  }
+
+  const decrementar = () => {
+    count.value--
+  }
+</script>
+
+<style scoped>
+#app{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+}
+h1{
+  font-size: 36pt;
+}
+p{
+  color: red;
+}
+</style>
