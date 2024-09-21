238e6261d940a4150a66a416359a160fccf14db2
arielrojas39 <rojas.ariel.397@gmail.com>
2024-09-20 22:02:32 -0300

    Agregamos un poco de estilos al componente App

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,8 +1,12 @@
 <template>
-  <h1>Hola mundo!</h1>
-  <p>{{count}}</p>
-  <button @click="incrementar">Incrementar</button>
-  <button @click="decrementar">Decrementar</button>
+  <div class="container">
+    <h1>Hola mundo!</h1>
+    <p>{{count}}</p>
+    <div class="buttons">
+      <button @click="incrementar">Incrementar</button>
+      <button @click="decrementar">Decrementar</button>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -18,17 +22,30 @@
 </script>
 
 <style scoped>
-#app{
+.container{
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: center;
-  width: 100vw;
-  height: 100vh;
+  align-items: center;  
+  background-color: rgb(4, 99, 68);
+  padding: 0;
+  margin:0;
+}
+.buttons{
+  display: flex;
+  gap:10pt;
+}
+button{
+  width:80pt;
+  height: 20pt;
+  border: none;
+  border-radius: 5pt;
+  background-color: hsla(160, 100%, 37%, 1);
 }
 h1{
   font-size: 36pt;
 }
 p{
-  color: red;
+  color: hsla(160, 100%, 37%, 1);;
 }
 </style>
